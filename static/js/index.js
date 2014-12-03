2cd23326a82df0523d652c60f54287a264bc75f8
chenjiasheng <chenjiasheng464@pingan.com.cn>
2014-12-03 16:21:05 +0800

    web update

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,11 @@
 $(function(){
-    $('#fullpage').fullpage({
+	var para = {
     	'verticalCentered': false,
-    	'css3': true
-    });
+    	'css3': false
+    }
+    var a = document.createElement("div");
+	if('transform' in a.style|| 'msTransform' in a.style|| 'webkitTransform' in a.style){
+		para.css3 = true;
+	}
+    $('#fullpage').fullpage(para);
 });
