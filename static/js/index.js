a3182fcbda78055a281969f68e645f40b4664947
chenjiasheng <chenjiasheng464@pingan.com.cn>
2014-12-04 10:14:20 +0800

    优化遮罩层及修改bug,优化初始化js

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,44 @@
 $(function(){
-	var para = {
-    	'verticalCentered': false,
-    	'css3': false
-    }
-    var a = document.createElement("div");
-	if('transform' in a.style|| 'msTransform' in a.style|| 'webkitTransform' in a.style){
-		para.css3 = true;
-	}
-    $('#fullpage').fullpage(para);
+	var fuc = {
+		init: function(){
+			this.initFullpage();
+			this.initEvent();
+		},
+		initMask : function(){
+			$('<div id="divprogressbar" style="position: fixed; width: 100%; height: 100%; left: 0px; top: 0px; background-color: #fff; opacity:1.0; filter: alpha (opacity = 100); z-index: 9999"><div class="loading-wrap"><img src="static/images/loading.gif"></img><p>玩命加载中……</p><p id="hint" style="display:none;">载入成功……</p></div></div>').appendTo($('body'));
+		},
+		initFullpage: function(){
+			var that = this;
+			var para = {
+		    	'verticalCentered': false,
+		    	'anchors': ['page1', 'page2', 'page3', 'page4'],
+		    	'css3': false,
+		    	'afterRender': function(){
+		    		that.initMask();
+		    	}
+		    }
+		    var ex = document.createElement("div");
+			if('transform' in ex.style|| 'msTransform' in ex.style|| 'webkitTransform' in ex.style|| 'oTransform' in ex.style){
+				para.css3 = true;
+			}
+		    $('#fullpage').fullpage(para);
+		},
+		initEvent: function(){
+			/*document.onreadystatechange = function () {   
+	            if(document.readyState=="complete") {
+	            	$("#hint").fadeIn();
+	            	setTimeout(function(){
+	            		document.getElementById('divprogressbar').style.display='none';
+	            	},5000)
+	            } 
+	        };*/
+	        window.onload = function(){
+            	$("#hint").fadeIn();
+            	setTimeout(function(){
+            		document.getElementById('divprogressbar').style.display='none';
+            	},3000)
+	        };
+		}
+	};
+	fuc.init();
 });
