48b658ad6baa320b52fe43a7ed97e7979a3259e8
chenjiasheng <chenjiasheng464@pingan.com.cn>
2015-04-17 16:34:38 +0800

    fix

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,7 @@ $(function(){
 	var fuc = {
 		init: function(){
 			this.initFullpage();
-			this.initEvent();
+			this.bindEvent();
 		},
 		initMask : function(){
 			$('<div id="divprogressbar" style="position: fixed; width: 100%; height: 100%; left: 0; top: 0; background-color: #fff; opacity:1.0; filter: alpha (opacity = 100); z-index: 9999"><div class="loading-wrap"><img src="static/images/loading.gif"></img><p class="css677e3e9e4bdae38e0b9e1aa6b6fa8f73">玩命加载中……</p><p id="hint" class="css677e3e9e4bdae38e0b9e1aa6b6fa8f73" style="display:none;">载入成功……</p></div></div>').appendTo($('body'));
@@ -14,7 +14,7 @@ $(function(){
 		    	'anchors': ['page1', 'page2', 'page3', 'page4'],
 		    	'css3': false,
 		    	'navigation': true,
-		    	'afterRender': function(){
+		    	'afterRender': function(anchor){
 		    		that.initMask();
 		    	}
 		    }
@@ -24,18 +24,22 @@ $(function(){
 			}
 		    $('#fullpage').fullpage(para);
 		},
-		initEvent: function(){
-	        $.fn.fullpage.setAutoScrolling(false);
+		bindEvent: function(){
+	        this.loadingMask();
+
+		},
+		loadingMask: function(){
+			$.fn.fullpage.setAutoScrolling(false);
 	        $('html, body').css({
 				'overflow' : 'hidden',
 				'height' : '100%'
 			});
 	        window.onload = function(){
-            	$("#hint").fadeIn();
-            	setTimeout(function(){
-            		$.fn.fullpage.setAutoScrolling(true);
-            		document.getElementById('divprogressbar').style.display='none';
-            	},3000);
+	        	$("#hint").fadeIn();
+	        	setTimeout(function(){
+	        		$.fn.fullpage.setAutoScrolling(true);
+	        		document.getElementById('divprogressbar').style.display='none';
+	        	},3000);
 	        };
 		}
 	};
