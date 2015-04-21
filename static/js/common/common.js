0d8e6cdef401fdca146dd3b28ccbecb76f548c08
chenjiasheng <chenjiasheng464@pingan.com.cn>
2015-04-21 17:22:50 +0800

    fix

--- static/js/common/common.js
+++ static/js/common/common.js
@@ -7,7 +7,7 @@
 
 	/*判断是否手机浏览*/
 	function checkMobile(support, url){
-		url = url || 'helloworld.html';
+		url = url || '/page/abc.html';
 		if(navigator.platform.indexOf('Win32') == -1){ 
 			if(!support){
 				location.href = url;
